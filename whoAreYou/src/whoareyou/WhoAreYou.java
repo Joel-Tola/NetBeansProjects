fdd5637376768d8d1bd0063d6e02442cb75effdd
Joel-Tola <y*********@gmail.com>
2024-02-27 12:03:14 +0000

    StarUsingBufferedReader

--- whoAreYou/src/whoareyou/WhoAreYou.java
+++ whoAreYou/src/whoareyou/WhoAreYou.java
@@ -4,6 +4,8 @@
  */
 package whoareyou;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.text.MessageFormat;
 import java.util.Scanner;
 
@@ -14,15 +16,18 @@ import java.util.Scanner;
 public class WhoAreYou {
 
     /**
-     * 1)	Create a new Project called whoAreYou.
-     * Write a piece of code that will prompt the user (i.e. output a question) for their name.
-     * Read in the name using a Scanner and output a message saying: 
+     * 1) Create a new Project called whoAreYou.
+     * Write a piece of code that will prompt the user (i.e. output a question) for
+     * their name.
+     * Read in the name using a Scanner and output a message saying:
      * “Oh, that is a very nice name, <user name goes here>”
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Who are you?");
         System.out.println(MessageFormat.format("Oh, that is a very nice name, {0}", readLine()));
+
     }
 
     private static String readLine() {
@@ -34,5 +39,25 @@ public class WhoAreYou {
         } finally {
             scanner.close();
         }
+    }
+
+    private static String readLineByBufferReader() throws Exception {
+        BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            return bufferReader.readLine();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private static int readNumberByBufferReader() {
+        BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            return Integer.parseInt(bufferReader.readLine());
+        } catch (Exception e) {
+            System.out.println("Please insert a valid number");
+            readNumberByBufferReader();
+        }
+        return 0;
     }
 }
